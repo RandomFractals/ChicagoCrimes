1bb1389fda0df71cc12b4388d1584b9a0fc64b77
Taras Novak <taras.novak@randomfractals.com>
2017-08-16 08:44:45 -0500

    added a note about conversion script taking an hour to parse 1.4Gb of data and convert it to parquet + timestamps for scirpt outputs

--- scripts/convert_crime_csv_to_parquet.py
+++ scripts/convert_crime_csv_to_parquet.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import dask.dataframe as dd
+from datetime import datetime
 
 def print_line():
     print('------------------------------------------------------------')    
@@ -9,14 +10,20 @@ def print_line():
 # load csv data into dask df
 # Note: change this to parse smaller ~33Mb 2017 crime data file
 data_file_name = '../raw_data/Crimes_-_2001_to_present.csv' #'../raw_data/Crimes_-_2017.csv'
-print('Loading data file: {} ...'.format(data_file_name))
+
+print('{}: Loading data file: {} ...'.format(\
+	datetime.now().strftime('%H:%M:%S'), data_file_name))
+print('\tThis might take an hour on a quad core with 4Gb or ram to spare')
+print('\tRun it before you step out for lunch :)\n...')
 df = dd.read_csv(data_file_name, 
                  error_bad_lines=False,
-                 assume_missing=True, # dtype={'Ward': int}) #dtype='str')
+                 assume_missing=True, # dtype={'Ward': int})
+								 dtype={'Location': 'str'},
                  parse_dates=['Date'], infer_datetime_format=True)
 
 # persist in memory
 df = df.persist()
+print('{}: All data loaded into memory.'.format(datetime.now().strftime('%H:%M:%S')))
 
 # log records count and load data partitions
 print_line()
@@ -52,16 +59,16 @@ def unique_column_values(df):
             df[column].dtype))
 
 # print unique column values counts
-print_line()
-print('Unique column values:')
-print_line()
-print('Name | Unique # | Type')
-unique_column_values(df)
+#print_line()
+#print('Unique column values:')
+#print_line()
+#print('Name | Unique # | Type')
+#unique_column_values(df)
 
 # reduce data set
 select_columns = ['Date', 'Block', 'PrimaryType',
                   'Description', 'LocationDescription', 'CommunityArea', 
-                  'Arrest', 'Domestic', 'Latitude', 'Longitude']
+                  'Arrest', 'Domestic', 'Latitude', 'Longitude', 'Year']
 df = df[select_columns]
 
 # print some stats
@@ -95,9 +102,12 @@ print_line()
 
 # save it in parquet format
 parquet_file_name = '../data/crimes-2001-to-present.snappy.parq' #'../data/crimes-2017.snappy.parq'
-print('Converting to parquet format: {} ...'.format(parquet_file_name))
+print('{}: Converting data to parquet format: {} ...'.format(\
+	datetime.now().strftime('%H:%M:%S'), parquet_file_name))
 print('...')
 df.to_parquet(parquet_file_name, compression='SNAPPY')
+print('{}: Finished creating snappy parquet dataset.'.format(datetime.now().strftime('%H:%M:%S')))
+
 
 
 
