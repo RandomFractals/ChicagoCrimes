103348ed39dd96d8c8f65e1a6117bf0223181a81
Taras Novak <taras.novak@randomfractals.com>
2017-08-19 05:34:42 -0500

    Optimized data trimming script o reduce 1.4Gb of data to 190Mb for faster loading in a notebook. Refactored raw csv data conversion logging.
    Set date index and data categories to shrink parquet data on disk.

--- scripts/convert_crime_csv_to_parquet.py
+++ scripts/convert_crime_csv_to_parquet.py
@@ -4,23 +4,71 @@ import pandas as pd
 import dask.dataframe as dd
 from datetime import datetime
 
+#---------------------------- Print Functions --------------------------------------------
+# prints a line separator
 def print_line():
-    print('------------------------------------------------------------')    
-    
-# load csv data into dask df
-# Note: change this to parse smaller ~35Mb 2017 crime data file
-data_file_name = '../raw_data/Crimes_-_2001_to_present.csv' #'../raw_data/Crimes_-_2017.csv'
+    print('-------------------------------------------------------------------------------')  
 
-print('{}: Loading data file (~48min): {} ...'\
-	.format(datetime.now().strftime('%H:%M:%S'), data_file_name))
-print('\tThis might take an hour on a quad core with 4Gb of ram to spare.')
-print('\tRun it before you step out for lunch :)\n...')
+# prints a msg with a timestamp
+def log(msg):
+	print('\n{}: {}'\
+		.format(datetime.now().strftime('%H:%M:%S'), msg))
+
+# prints dataframe info
+def print_dataframe_info(df):
+	print('\nDataFrame info:')
+	print_line()
+	print(df.info())
+	print_line()
+	
+# prints # of records and partitions in a dataframe
+def print_dataframe_size(df):
+	print('{:,} total records in {} partitions'\
+		.format(len(df), df.npartitions))
+	print('DataFrame size: {:,}'.format(df.size.compute()))
+	print_line()
+
+# prints dataframe columns and dtypes
+def print_columns(df):
+	print('Columns:')
+	print_line()
+	print(df.columns)
+	print_line()
+	print('\nColumn dtypes:')
+	print_line()
+	print(df.dtypes)
+	print_line()
+
+# prints unique column value counts
+def print_unique_column_values(df):
+	print('\nUnique column values...')
+	print_line()
+	print('Name | Unique # | Type')
+	for column in df.columns:
+		print('{} | {} | {}'.format(
+			df[column].name,
+			len(df[column].unique()),
+			df[column].dtype))
+	print_line()
+
+#---------------------------- Data Processing Script ------------------------------
+
+# load csv data into dask df
+# Note: change this to parse smaller ~36Mb 2017 crime data file
+data_file_name = '../raw_data/Crimes_-_2001_to_present.csv' #'../raw_data/Crimes_-_2017.csv'
+log('Loading data file: {} ...'.format(data_file_name))
+if data_file_name.endswith('Crimes_-_2001_to_present.csv'):
+	print('This might take half an hour on a quad core with fast SDD and 4Gb of ram to spare.')
+	print('\tRun it before you step out for lunch :)\n...')
+else:
+	print('\tGive it a few minutes to load raw data. Go grab some coffee.\n...')
 
 # load csv data
 df = dd.read_csv(data_file_name, 
                  error_bad_lines=False,
                  assume_missing=True,
-								 dtype={'ID': np.int64, 
+								 dtype={'ID': np.int64,
+									'PrimaryType': 'str',
 									'Beat': np.uint16, 
 									#'District': 'uint16', 
 									#'Ward': 'uint16',
@@ -30,107 +78,57 @@ df = dd.read_csv(data_file_name,
                  parse_dates=['Date'], 
 								 infer_datetime_format=True)
 
-# persist data frame in memory
+# persist dataframe in memory
 df = df.persist()
+log('All data loaded into memory.')
 
-print('{}: All data loaded into memory.'\
-	.format(datetime.now().strftime('%H:%M:%S')))
+# print raw data stats
+print_dataframe_info(df)
+print_columns(df)
+if data_file_name.endswith('Crimes_-_2017.csv'):
+	print_unique_column_values(df) # for small data sets only
+print_dataframe_size(df)
 
-# log records count and load data partitions
-print_line()
-print('Data set info:')
-print_line()
-print('{:,} total records in {} partitions'\
-	.format(len(df), df.npartitions))
-
-# drop duplicates
+# drop duplicate cases
+log('Dropping duplicate cases...')
 df.drop_duplicates(subset=['ID', 'Case Number'], inplace=True)
-
-print('DataFrame size: {:,}'.format(df.size.compute()))
 
 # strip out white space from column names
 df = df.rename(columns={c: c.replace(' ', '') for c in df.columns})
 
-# list columns
-print_line()
-print('Columns:')
-print_line()
-print(df.columns)
-
-# infer data types
-print_line()
-print('Column dtypes:')
-print_line()
-print(df.dtypes)
-
-def unique_column_values(df):
-  for column in df.columns:
-    print('{} | {} | {}'.format(
-      df[column].name,
-      len(df[column].unique()),
-      df[column].dtype))
-
-# print unique column values counts
-#print_line()
-#print('Unique column values:')
-#print_line()
-#print('Name | Unique # | Type')
-#unique_column_values(df)
-
-# reduce data set
+# reduce dataframe to the select columns we need for charting crime stats
+log('Dropping some columns...')
 select_columns = ['Date', 'Block', 'PrimaryType',
                   'Description', 'LocationDescription', 'CommunityArea', 
                   'Arrest', 'Domestic', 'Latitude', 'Longitude', 'Year']
 df = df[select_columns]
+#df = df.drop(['ID', 'CaseNumber', 'IUCR', 'Beat', 'District', 'Ward',\
+#	'FBICode', 'UpdatedOn', 'Location'], axis=1) # denotes column
 
-# print some dataset stats
-print_line()
-print('{}: Dataset stats...'\
-	.format(datetime.now().strftime('%H:%M:%S')))
-print_line()
-print('{:,} total records'.format(len(df)))
-
-# drop duplicates
-print('Dropping duplicates...')
+# drop duplicate records
+log('Dropping duplicate records...')
 df = df.drop_duplicates() #.dropna()
 print('{:,} unique records'.format(len(df)))
 
-print('Calculating total arrests and domestic crime reports... (~3 min)')
-
-# count arrests
-arrests_df = df[df.Arrest==True]
-print('{:,} arrests'.format(len(arrests_df)))
-
-# count domestic crime reports
-domestic_df = df[df.Domestic==True]
-print('{:,} domestic crime reports'.format(len(domestic_df)))
-
-# print data frame info
-print_line()
-print('{}: DataFrame info():'\
-	.format(datetime.now().strftime('%H:%M:%S')))
-print_line()
-print(df.info())
-
 # list reduced dataset columns
-print_line()
-print('Reduced dataset Columns:')
-print_line()
-print(df.columns)
-print_line()
+log('Reduced DataFrame Columns:')
+print_columns(df)
 
-# save it in parquet format
+# set Date index
+log('Creating Date index...')
+df = df.set_index('Date')
+
+# create data categories to shrink data size on disk
+log('Creating data categories...')
+df = df.categorize(columns=['PrimaryType', 'Description', 'LocationDescription', 'CommunityArea', 'Year'])
+
+# print data frame info before save
+log('Reduced DataFrame info...')
+print_dataframe_info(df)
+
+# save dataframe in compressed snappy parquet format
 parquet_data_folder = '../data/crimes-2001-to-present.snappy.parq' #'../data/crimes-2017.snappy.parq'
-print('{}: Saving data in snappy parquet format to (~6 min): {} ...'\
-	.format(datetime.now().strftime('%H:%M:%S'), parquet_data_folder))
+log('Saving data in snappy parquet format to: {} ...'.format(parquet_data_folder))
 print('...')
-df.to_parquet(parquet_data_folder, compression='SNAPPY')
-print('{}: Finished creating snappy parquet dataset at: {}'\
-	.format(datetime.now().strftime('%H:%M:%S'), parquet_data_folder))
-
-
-
-
-
-
-
+df.to_parquet(parquet_data_folder, compression='SNAPPY') #, partition_on=['Year']) #,write_index=True)
+log('Finished creating snappy parquet dataset at: {}'.format(parquet_data_folder))
