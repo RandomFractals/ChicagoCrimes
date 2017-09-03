ff085270dd5d8e6728ae232ddb2af54c126317e9
Taras Novak <taras.novak@randomfractals.com>
2017-09-03 08:57:06 -0500

    add Beat, District and Ward to parquet data for mapping those Chicago divisions

--- scripts/convert_crime_csv_to_parquet.py
+++ scripts/convert_crime_csv_to_parquet.py
@@ -54,7 +54,7 @@ def print_unique_column_values(df):
 #---------------------------- Data Processing Script ---------------------------------------
 
 # load csv data into dask df
-# Note: change this to parse smaller ~36Mb 2017 crime data file included in this repo
+# Note: change this to parse smaller ~39Mb 2017 crime data file included in this repo
 data_file_name = '../raw_data/Crimes_-_2001_to_present.csv' #'../data/Crimes_-_2017.csv'
 log('Loading data file: {} ...'.format(data_file_name))
 if data_file_name.endswith('Crimes_-_2001_to_present.csv'):
@@ -67,16 +67,17 @@ else:
 df = dd.read_csv(data_file_name, 
                  error_bad_lines=False,
                  assume_missing=True,
-								 dtype={'ID': np.int64,
-									'PrimaryType': 'str',
-									'Beat': np.uint16, 
-									#'District': 'uint16', 
-									#'Ward': 'uint16',
-									#'Community Area': 'uint16',
-									'Year': np.uint16, 
-									'Location': 'str'},
-                 parse_dates=['Date'], 
-								 infer_datetime_format=True)
+				 dtype={'ID': np.int64,
+				 'PrimaryType': 'str',
+				 'FBICode': 'str',
+				 'Beat': np.uint16,
+				 #'District': np.uint16,
+				 #'Ward': np.uint16,
+				 #'Community Area': 'uint16',
+				 'Year': np.uint16,
+				 'Location': 'str'},
+                 parse_dates=['Date'],
+				 infer_datetime_format=True)
 
 # persist dataframe in memory
 df = df.persist()
@@ -98,8 +99,9 @@ df = df.rename(columns={c: c.replace(' ', '') for c in df.columns})
 
 # reduce dataframe to the select columns we need for charting crime stats
 log('Dropping some columns...')
-select_columns = ['Date', 'Block', 'PrimaryType',
-                  'Description', 'LocationDescription', 'CommunityArea', 
+select_columns = ['Date', 'Block', 'PrimaryType', 'FBICode',
+                  'Description', 'LocationDescription', 
+				  'CommunityArea', 'Beat', 'District', 'Ward',
                   'Arrest', 'Domestic', 'Latitude', 'Longitude', 'Year']
 df = df[select_columns]
 #df = df.drop(['ID', 'CaseNumber', 'IUCR', 'Beat', 'District', 'Ward',\
@@ -120,7 +122,9 @@ df = df.set_index('Date')
 
 # create data categories to shrink data size on disk
 log('Creating data categories...')
-df = df.categorize(columns=['PrimaryType', 'Description', 'LocationDescription', 'CommunityArea', 'Year'])
+df = df.categorize(columns=['PrimaryType', 'FBICode', 
+	'Description', 'LocationDescription', 
+	'CommunityArea', 'Beat', 'District', 'Ward', 'Year'])
 
 # print data frame info before save
 log('Reduced DataFrame info...')
