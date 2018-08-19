63d9dc210652727c2fe2f9319332d43b6706643c
Taras Novak <taras.novak@gmail.com>
2018-08-19 17:20:44 -0600

    added daily homicides breakdown to chicago-homicides.js notebook

--- notebooks/observablehq/chicago-homicides.js
+++ notebooks/observablehq/chicago-homicides.js
@@ -1,11 +1,11 @@
 // URL: https://beta.observablehq.com/@randomfractals/chicago-homicides
 // Title: Chicago Homicides
 // Author: Taras Novak (@randomfractals)
-// Version: 93
+// Version: 110
 // Runtime version: 1
 
 const m0 = {
-  id: "2c207c68a127a60d@93",
+  id: "2c207c68a127a60d@110",
   variables: [
     {
       inputs: ["md"],
@@ -46,6 +46,31 @@ d3.csv(dataUrl)
 )})
     },
     {
+      name: "dailyHomicides",
+      inputs: ["groupByDay","homicides"],
+      value: (function(groupByDay,homicides){return(
+groupByDay(homicides)
+)})
+    },
+    {
+      name: "groupByDay",
+      value: (function(){return(
+function groupByDay(dataArray) {
+  let daily = {};
+  dataArray.map(data => {
+    const date = new Date(data.Date);
+    const timezoneOffset = date.getTimezoneOffset() * 60 * 1000;
+    const day = new Date(date.getTime() - timezoneOffset).toISOString().substring(0, 10); // ISO date string;
+    if (!daily[day]) {
+      daily[day] = [];
+    }
+    daily[day].push(data);
+  });
+  return daily;
+}
+)})
+    },
+    {
       name: "d3",
       inputs: ["require"],
       value: (function(require){return(
@@ -56,7 +81,7 @@ require('d3')
 };
 
 const notebook = {
-  id: "2c207c68a127a60d@93",
+  id: "2c207c68a127a60d@110",
   modules: [m0]
 };
 
