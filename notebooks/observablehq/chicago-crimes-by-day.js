7cdde07183cfff84b63016bd4c873d3eeda615b8
Taras Novak <taras.novak@randomfractals.com>
2018-08-19 08:25:40 -0500

    added crime type selectro to chicago-crimes-by-day.js notebook

--- notebooks/observablehq/chicago-crimes-by-day.js
+++ notebooks/observablehq/chicago-crimes-by-day.js
@@ -1,35 +1,44 @@
 // URL: https://beta.observablehq.com/@randomfractals/chicago-crimes-by-day
 // Title: Chicago Crimes by Day
 // Author: Taras Novak (@randomfractals)
-// Version: 407
+// Version: 426
 // Runtime version: 1
 
 const m0 = {
-  id: "e901da8a28f3f554@407",
+  id: "e901da8a28f3f554@426",
   variables: [
     {
       inputs: ["md"],
       value: (function(md){return(
-md`# Chicago Crimes by Day
-
-*mouseover to see the number of reported crimes per day*`
+md `# Chicago Crimes by Day`
 )})
     },
     {
-      name: "viewof weekday",
-      inputs: ["html"],
-      value: (function(html){return(
-html`<select>
-  <option value=monday>Monday-based weeks
-  <option value=sunday>Sunday-based weeks
-  <option value=weekday>Weekdays only
-</select>`
+      name: "viewof crimeType",
+      inputs: ["select"],
+      value: (function(select){return(
+select(['', 'HOMICIDE', 'KIDNAPPING', 'NARCOTICS', 'PROSTITUTION', 'ARSON', 
+                          'THEFT', 'BATTERY', 'CRIMINAL DAMAGE', 'ASSAULT', 'SEX OFFENSE',
+                          'OTHER OFFENSE', 'DECEPTIVE PRACTICE',
+                          'BURGLARY', 'MOTOR VEHICLE THEFT', 'ROBBERY',
+                          'CRIMINAL TRESPASS', 'WEAPONS VIOLATION',
+                          'OFFENSE INVOLVING CHILDREN',
+                          'PUBLIC PEACE VIOLATION',
+                          'CRIM SEXUAL ASSAULT',
+                          'INTERFERENCE WITH PUBLIC OFFICER',
+                          'LIQUOR LAW VIOLATION', 'STALKING', 'GAMBLING'])
 )})
     },
     {
-      name: "weekday",
-      inputs: ["Generators","viewof weekday"],
+      name: "crimeType",
+      inputs: ["Generators","viewof crimeType"],
       value: (G, _) => G.input(_)
+    },
+    {
+      inputs: ["md"],
+      value: (function(md){return(
+md `*mouseover to see the number of reported crimes per day*`
+)})
     },
     {
       name: "chart",
@@ -99,6 +108,22 @@ html`<select>
   return svg.node();
 }
 )
+    },
+    {
+      name: "viewof weekday",
+      inputs: ["html"],
+      value: (function(html){return(
+html`<select>
+  <option value=monday>Monday-based weeks
+  <option value=sunday>Sunday-based weeks
+  <option value=weekday>Weekdays only
+</select>`
+)})
+    },
+    {
+      name: "weekday",
+      inputs: ["Generators","viewof weekday"],
+      value: (G, _) => G.input(_)
     },
     {
       name: "cellSize",
@@ -209,9 +234,9 @@ loadData(dataUrl).then(buffer => arrow.Table.from(new Uint8Array(buffer)))
     },
     {
       name: "dailyData",
-      inputs: ["groupByDay","dataTable"],
-      value: (function(groupByDay,dataTable){return(
-groupByDay(dataTable)
+      inputs: ["groupByDay","dataTable","crimeType"],
+      value: (function(groupByDay,dataTable,crimeType){return(
+groupByDay(dataTable, crimeType)
 )})
     },
     {
@@ -234,13 +259,15 @@ Object.keys(dailyData)
       name: "groupByDay",
       inputs: ["arrow","toDate"],
       value: (function(arrow,toDate){return(
-function groupByDay(data) {
+function groupByDay(dataTable, crimeType) {
   let groupData, date, location, arrested, info, results = {};
-  const dateFilter = arrow.predicate.custom(i => {
-    const date = toDate(data.getColumn('Date').get(i));
-    return (date.getMonth() <= 6); // through June
+  const dataFilter = arrow.predicate.custom(i => {
+    const date = toDate(dataTable.getColumn('Date').get(i));
+    const primaryType = dataTable.getColumn('PrimaryType').get(i);    
+    return ((crimeType === '' || primaryType === crimeType) &&
+      date.getMonth() <= 8); // through August
   }, b => 1);
-  data.filter(dateFilter)  
+  dataTable.filter(dataFilter)  
   .scan((index) => {
     const day = toDate(date(index)).toISOString().substring(0, 10); // ISO date string
     if (!results[day]) {
@@ -278,6 +305,11 @@ function groupByDay(data) {
       value: (function(require){return(
 require('apache-arrow')
 )})
+    },
+    {
+      from: "@jashkenas/inputs",
+      name: "select",
+      remote: "select"
     }
   ]
 };
@@ -308,9 +340,110 @@ function toDate(timestamp) {
   ]
 };
 
+const m2 = {
+  id: "@jashkenas/inputs",
+  variables: [
+    {
+      name: "select",
+      inputs: ["input","html"],
+      value: (function(input,html){return(
+function select(config = {}) {
+  let {
+    value: formValue,
+    title,
+    description,
+    submit,
+    multiple,
+    size,
+    options
+  } = config;
+  if (Array.isArray(config)) options = config;
+  options = options.map(
+    o => (typeof o === "object" ? o : { value: o, label: o })
+  );
+  const form = input({
+    type: "select",
+    title,
+    description,
+    submit,
+    getValue: input => {
+      const selected = Array.prototype.filter
+        .call(input.options, i => i.selected)
+        .map(i => i.value);
+      return multiple ? selected : selected[0];
+    },
+    form: html`
+      <form>
+        <select name="input" ${
+          multiple ? `multiple size="${size || options.length}"` : ""
+        }>
+          ${options.map(
+            ({ value, label }) => `
+            <option value="${value}" ${
+              value === formValue ? "selected" : ""
+            }>${label}</option>
+          `
+          )}
+        </select>
+      </form>
+    `
+  });
+  form.output.remove();
+  return form;
+}
+)})
+    },
+    {
+      name: "input",
+      inputs: ["html","d3format"],
+      value: (function(html,d3format){return(
+function input(config) {
+  let {form, type = "text", attributes = {}, action, getValue, title, description, format, submit, options} = config;
+  if (!form) form = html`<form>
+	<input name=input type=${type} />
+  </form>`;
+  const input = form.input;
+  Object.keys(attributes).forEach(key => {
+    const val = attributes[key];
+    if (val != null) input.setAttribute(key, val);
+  });
+  if (submit) form.append(html`<input name=submit type=submit style="margin: 0 0.75em" value="${typeof submit == 'string' ? submit : 'Submit'}" />`);
+  form.append(html`<output name=output style="font: 14px Menlo, Consolas, monospace; margin-left: 0.5em;"></output>`);
+  if (title) form.prepend(html`<div style="font: 700 0.9rem sans-serif;">${title}</div>`);
+  if (description) form.append(html`<div style="font-size: 0.85rem; font-style: italic;">${description}</div>`);
+  if (format) format = d3format.format(format);
+  if (action) {
+    action(form);
+  } else {
+    const verb = submit ? "onsubmit" : type == "button" ? "onclick" : type == "checkbox" || type == "radio" ? "onchange" : "oninput";
+    form[verb] = (e) => {
+      e && e.preventDefault();
+      const value = getValue ? getValue(input) : input.value;
+      if (form.output) form.output.value = format ? format(value) : value;
+      form.value = value;
+      if (verb !== "oninput") form.dispatchEvent(new CustomEvent("input"));
+    };
+    if (verb !== "oninput") input.oninput = e => e && e.stopPropagation() && e.preventDefault();
+    if (verb !== "onsubmit") form.onsubmit = (e) => e && e.preventDefault();
+    form[verb]();
+  }
+  return form;
+}
+)})
+    },
+    {
+      name: "d3format",
+      inputs: ["require"],
+      value: (function(require){return(
+require("d3-format")
+)})
+    }
+  ]
+};
+
 const notebook = {
-  id: "e901da8a28f3f554@407",
-  modules: [m0,m1]
+  id: "e901da8a28f3f554@426",
+  modules: [m0,m1,m2]
 };
 
 export default notebook;
